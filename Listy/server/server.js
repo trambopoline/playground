7be4562744b619ba2e9d60d2c82d59b5bf85012d
Devin Dodson <Devin Dodson>
2018-03-27 17:57:00 -0700

    Database and table initialization logic

--- Listy/server/server.js
+++ Listy/server/server.js
@@ -3,31 +3,41 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-require('dotenv').config();
-
 const rdb = require('rethinkdb');
+require('rethinkdb-init')(rdb);
+require('dotenv').config();
+// const rdb = require('./database');
 const app = express();
 
 const port = process.env.SERVER_PORT;
 const host = process.env.SERVER_HOST;
 
-//Database
 const dbPort = process.env.DB_PORT;
 const dbName = process.env.DB_NAME;
 const listTableName = 'listItem';
 let connection = null;
 
-rdb.connect({ host, port: dbPort, db: dbName }, function (err, conn)
-{
-    if (err) throw err;
-    connection = conn;
-});
-// rdb.db(dbName).tableCreate('listy').run(connection, function (err, result)
-// {
-//     if (err) throw err;
-//     console.log(JSON.stringify(result, null, 2));
-// });
+//Create the database and table if it doesn't exist already
+rdb.init({
+    host,
+    port: dbPort,
+    db: dbName
+},
+    [listTableName])
+    .then(function (conn)
+    {
+        console.log(conn)
+    });
 
+rdb.connect({
+    host,
+    port: dbPort,
+    db: dbName
+}, function (err, conn)
+    {
+        if (err) throw err;
+        connection = conn;
+    });
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
@@ -50,11 +60,9 @@ const server = app.listen(port, function () {
 
 // Get the entire list
 app.get('/list', function (req, res) {
-    rdb.table(listTableName).run(connection, function (err, cursor)
-    {
+    rdb.table(listTableName).run(connection, function (err, cursor) {
         if (err) throw err;
-        cursor.toArray(function (err, result)
-        {
+        cursor.toArray(function (err, result) {
             if (err) throw err;
             res.json(result);
         });
@@ -64,10 +72,9 @@ app.get('/list', function (req, res) {
 // Get specific list item
 app.get('/list/:listItemKey', function (req, res) {
     const listItemKey = req.params.listItemKey;
-    rdb.table(listTableName).get(listItemKey).run(connection, function (err, result)
-    {
+    rdb.table(listTableName).get(listItemKey).run(connection, function (err, result) {
         if (err) throw err;
-        if( result ){
+        if (result) {
             res.json(result);
             return;
         }
@@ -80,18 +87,17 @@ app.get('/list/:listItemKey', function (req, res) {
 // Create a new list item
 app.post('/list', function (req, res) {
     const newListItem = req.body.content;
-    if( !newListItem )
-    {
+    if (!newListItem) {
         res.status(400).json({
             "error": `Submitted list items must have a non-empty 'content' field`
         });
         return;
     }
-    rdb.table(listTableName).insert({ "content": newListItem }).run(connection, function (err, result)
-    {
+    rdb.table(listTableName).insert({
+        "content": newListItem
+    }).run(connection, function (err, result) {
         if (err) throw err;
-        if (result.inserted === 1)
-        {
+        if (result.inserted === 1) {
             res.json(result.generated_keys[0]);
             // console.log(result);
             return;
@@ -103,18 +109,17 @@ app.post('/list', function (req, res) {
 app.put('/list/:listItemKey', function (req, res) {
     const listItemKey = req.params.listItemKey;
     const updatedListItem = req.body.content;
-    if (!updatedListItem)
-    {
+    if (!updatedListItem) {
         res.status(400).json({
             "error": `Submitted list items must have a non-empty 'content' field`
         });
         return;
     }
-    rdb.table(listTableName).get(listItemKey).update({ "content": updatedListItem }).run(connection, function (err, result)
-    {
+    rdb.table(listTableName).get(listItemKey).update({
+        "content": updatedListItem
+    }).run(connection, function (err, result) {
         if (err) throw err;
-        if (result.replaced === 1 || result.unchanged === 1)
-        {
+        if (result.replaced === 1 || result.unchanged === 1) {
             res.json(listItemKey);
             return;
         }
@@ -129,11 +134,9 @@ app.put('/list/:listItemKey', function (req, res) {
 app.delete('/list/:listItemKey', function (req, res) {
     const listItemKey = req.params.listItemKey;
 
-    rdb.table(listTableName).get(listItemKey).delete().run(connection, function (err, result)
-    {
+    rdb.table(listTableName).get(listItemKey).delete().run(connection, function (err, result) {
         if (err) throw err;
-        if (result.deleted === 1)
-        {
+        if (result.deleted === 1) {
             res.json(listItemKey);
             return;
         }
